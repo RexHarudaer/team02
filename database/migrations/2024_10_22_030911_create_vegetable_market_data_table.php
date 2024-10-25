9d4468c64c2e148071c7f592b767907b2bd128b4
Jieice0 <0906776103j@gmail.com>
2024-10-25 13:34:47 +0800

    表格欄位高度設置移除

--- database/migrations/2024_10_22_030911_create_vegetable_market_data_table.php
+++ database/migrations/2024_10_22_030911_create_vegetable_market_data_table.php
@@ -16,13 +16,13 @@ class CreateVegetableMarketDataTable extends Migration
         Schema::create('vegetable_market_data', function (Blueprint $table) {
             $table->id(); // 自增主鍵
             $table->integer('year')->comment('年份'); // 年份
-            $table->string('type', 50)->comment('類別'); // 類別
-            $table->string('category', 50)->comment('植物名稱'); // 植物名稱
-            $table->double('plant_picture', 5, 2)->comment('植物圖片價格'); // 植物圖片價格
-            $table->double('total_average_price', 5, 2)->comment('總平均價格'); // 總平均價格
+            $table->string('type')->comment('類別'); // 類別
+            $table->string('cate')->comment('植物名稱'); // 植物名稱
+            $table->double('plant_picture')->comment('植物圖片價格'); // 植物圖片價格
+            $table->double('total_average_price')->comment('總平均價格'); // 總平均價格
             $table->integer('total_yield')->comment('總產量'); // 總產量
-            $table->string('market', 50)->comment('市場名稱'); // 市場名稱
-            $table->double('average_price', 5, 2)->comment('市場的平均價格'); // 市場的平均價格
+            $table->string('market')->comment('市場名稱'); // 市場名稱
+            $table->double('average_price')->comment('市場的平均價格'); // 市場的平均價格
             $table->integer('yield')->comment('市場的產量'); // 市場的產量
             $table->timestamps(); // 自動生成的 created_at 和 updated_at 欄位
         });

d04bbe549de3322642ecdee0b24782e5e9360553
Jieice0 <0906776103j@gmail.com>
2024-11-05 10:29:21 +0800

    隨機生成資料統整完成

--- database/seeders/VegetableMarketDataTableSeeder.php
+++ database/seeders/VegetableMarketDataTableSeeder.php
@@ -17,7 +17,7 @@ class VegetableMarketDataTableSeeder extends Seeder
      
     public function run()
     {
-      for ($i = 0; $i < 10; $i++)
+      for ($i = 0; $i < 25; $i++)
       {
         $years = [102, 103];
         $markets = ['三重市', '三重區', '台中市', '台中市場', '台北一', '台北二', '台北市場', '台東市', '台南市場', '永靖鄉', '西螺鎮', '宜蘭市', '東勢區', '東勢鎮', '花蓮市', '南投市', '屏東市', '桃園縣', '高雄市', '高雄市場', '溪湖鎮', '嘉義市', '彰化市場', '鳳山市', '鳳山區'];
@@ -26,7 +26,7 @@ class VegetableMarketDataTableSeeder extends Seeder
         '花卉類' => ['文心蘭', '康乃馨', '百合', '唐昌蒲', '火鶴', '玫瑰', '菊花'],
         '稻米及雜糧作物' => ['甘藷', '落花生']];
         $selectedType = array_rand($types);
-        // 根據類別選擇相應的植物名稱
+        // 根據類別選擇相應的植物種類名稱
         $selectedPlantName = $types[$selectedType][array_rand($types[$selectedType])];
         DB::table('vegetable_market_data')->insert([
             [

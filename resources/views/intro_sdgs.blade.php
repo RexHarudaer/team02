efba8bbf0a6c0f70075dce7f56e4bd667956e05d
RexHarudaer <aaaaa0973633561@gmail.com>
2024-11-26 09:32:33 +0800

    分割

--- resources/views/intro_sdgs.blade.php
+++ resources/views/intro_sdgs.blade.php
@@ -30,6 +30,20 @@
     </style>
 </head>
 <body>
+    <!-- Header -->
+  <header>
+    <div class="logo">
+      <h1>農業市場調查</h1>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="http://127.0.0.1:8001/">首頁</a></li>
+        <li><a href="http://127.0.0.1:8001/VegetableMarketDataController">表格</a></li>
+        <!-- <li><a href="#">服務</a></li>
+        <li><a href="#">聯繫我們</a></li>-->
+      </ul>
+    </nav>
+  </header>
     <header>
         <h1>聯合國永續發展目標 (SDGs)</h1>
         <p>17個全球永續發展的共同目標</p>
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('VegetableMarketDataView.app');
+});
+Route::get('/sdgs', function () {
+    return view('intro_sdgs');
 });
 
 
